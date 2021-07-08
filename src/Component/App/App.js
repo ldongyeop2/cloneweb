ec31e36c22224c07d7191ff57ff62602a36363db
ldongyeop2 <ldongyeop2@gmail.com>
2021-07-09 00:04:57 +0900

    react-app

---
+++ src/Component/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import '.header';
+import React, {Component} from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <header>
+
+            </header>
+        );
+    }
+}
+
+
+export default App;

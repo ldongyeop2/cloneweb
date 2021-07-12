bc8f68e4074d49913b7408ab6ea7e2983167bd58
ldongyeop2 <ldongyeop2@gmail.com>
2021-07-12 22:07:41 +0900

    App.js 컴포넌트 구조 수정

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,39 @@
-import './App.css';
-import '/Component/header';
-import '/Component/footer';
-import '/Component/article';
-import '/Component/aside';
 import React, {Component} from "react";
+import './App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>WEB</h1>
+                world wide web!
+            </header>
+        );
+    }
+}
+
+
+class TOC extends Component {
+    render() {
+        return (
+            <nav>
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
+            </nav>
+        );
+    }
+}
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header></header>
-                <aside></aside>
-                <main>
-                    <article></article>
-                    <article></article>
-                    <article></article>
-                </main>
-                <footer></footer>
+                <Header>
+                    <TOC>
+                    </TOC>
+                </Header>
             </div>
         );
     }
